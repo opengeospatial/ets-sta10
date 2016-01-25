97cfd1c8421c218210860c47e6b4e3dd62dc4234
Tania <tania.khalafbeigi@sensorup.com>
2016-01-25 11:18:43 -0700

    Add comments to Capability1Tests for API doc.

--- src/main/java/org/opengis/cite/sta10/sensingCore/Capability1Tests.java
+++ src/main/java/org/opengis/cite/sta10/sensingCore/Capability1Tests.java
@@ -17,13 +17,23 @@ import org.testng.annotations.Test;
 
 
 /**
- * Includes various tests of capability 1.
+ * Includes various tests of "A.1 Sensing Core" Conformance class.
  */
 public class Capability1Tests {
 
+    /**
+     * The root URL of the SensorThings service under the test
+     */
     public String rootUri;//="http://localhost:8080/OGCSensorThings-NewQueries/v1.0";
+    /**
+     * The variable tha defines to which recursive level the resource path should be tested
+     */
     private final int resourcePathLevel = 4;
 
+    /**
+     * This method will be run before starting the test for this conformance class. It cleans the database to start test.
+     * @param testContext The test context to find out whether thsi class is requested to test or not
+     */
     @BeforeClass
     public void obtainTestSubject(ITestContext testContext) {
         Object obj = testContext.getSuite().getAttribute(
@@ -43,6 +53,10 @@ public class Capability1Tests {
 
     }
 
+    /**
+     * This method is testing GET entities.
+     * It should return 200. Then the response entities are tested for control information, mandatory properties, and mandatory related entities.
+     */
     @Test(description = "GET Entities", groups = "level-1")
     public void readEntitiesAndCheckResponse() {
         String response = getEntities(EntityType.THING);
@@ -63,6 +77,10 @@ public class Capability1Tests {
         checkEntitiesAllAspectsForResponse(EntityType.FEATURE_OF_INTEREST, response);
     }
 
+    /**
+     * This method is testing GET when requesting a nonexistent entity.
+     * The response should be 404.
+     */
     @Test(description = "GET nonexistent Entity", groups = "level-1")
     public void readNonexistentEntity() {
         readNonexistentEntityWithEntityType(EntityType.THING);
@@ -75,6 +93,10 @@ public class Capability1Tests {
         readNonexistentEntityWithEntityType(EntityType.FEATURE_OF_INTEREST);
     }
 
+    /**
+     * This method is testing GET for a specific entity with its id.
+     * It checks the control information, mandatory properties and mandatory related entities for the response entity.
+     */
     @Test(description = "GET Specific Entity", groups = "level-1")
     public void readEntityAndCheckResponse() {
         String response = readEntityWithEntityType(EntityType.THING);
@@ -95,7 +117,10 @@ public class Capability1Tests {
         checkEntityAllAspectsForResponse(EntityType.FEATURE_OF_INTEREST, response);
     }
 
-    @Test(description = "GET Propety of an Entity", groups = "level-1")
+    /**
+     * This method is testing GET for a property of an entity.
+     */
+    @Test(description = "GET Property of an Entity", groups = "level-1")
     public void readPropertyOfEntityAndCheckResponse(){
         readPropertyOfEntityWithEntityType(EntityType.THING);
         readPropertyOfEntityWithEntityType(EntityType.LOCATION);
@@ -107,7 +132,11 @@ public class Capability1Tests {
         readPropertyOfEntityWithEntityType(EntityType.FEATURE_OF_INTEREST);
     }
 
-    public void readPropertyOfEntityWithEntityType(EntityType entityType) {
+    /**
+     * This helper method is testing property and property/$value for single entity of a given entity type
+     * @param entityType Entity type from EntityType enum list
+     */
+    private void readPropertyOfEntityWithEntityType(EntityType entityType) {
         try {
             String response = getEntities(entityType);
             Long id = new JSONObject(response).getJSONArray("value").getJSONObject(0).getLong(ControlInformation.ID);
@@ -121,7 +150,13 @@ public class Capability1Tests {
         }
     }
 
-    public void checkGetPropertyOfEntity(EntityType entityType, long id, String property){
+    /**
+     * This helper method sending GET request for requesting a property and check the response is 200.
+     * @param entityType Entity type from EntityType enum list
+     * @param id The id of the entity
+     * @param property The property to get requested
+     */
+    private void checkGetPropertyOfEntity(EntityType entityType, long id, String property){
         try {
             Map<String,Object> responseMap = getEntity(entityType, id, property);
             int responseCode = Integer.parseInt(responseMap.get("response-code").toString());
@@ -141,7 +176,13 @@ public class Capability1Tests {
         }
     }
 
-    public void checkGetPropertyValueOfEntity(EntityType entityType, long id, String property) {
+    /**
+     * This helper method sending GET request for requesting a property $value and check the response is 200.
+     * @param entityType Entity type from EntityType enum list
+     * @param id The id of the entity
+     * @param property The property to get requested
+     */
+    private void checkGetPropertyValueOfEntity(EntityType entityType, long id, String property) {
         Map<String,Object> responseMap = getEntity(entityType, id, property+"/$value");
         int responseCode = Integer.parseInt(responseMap.get("response-code").toString());
         Assert.assertEquals(responseCode, 200, "Reading property value of \"" + property + "\" of the exitixting " + entityType.name() + " with id " + id + " failed.");
@@ -153,7 +194,10 @@ public class Capability1Tests {
         }
     }
 
-    @Test(description = "GET Related Entity of an Entity", groups = "level-1")
+    /**
+     * This method is testing the resource paths based on specification to the specified level.
+     */
+    @Test(description = "Check Resource Path", groups = "level-1")
     public void checkResourcePaths(){
         readRelatedEntityOfEntityWithEntityType(EntityType.THING);
         readRelatedEntityOfEntityWithEntityType(EntityType.LOCATION);
@@ -165,7 +209,12 @@ public class Capability1Tests {
         readRelatedEntityOfEntityWithEntityType(EntityType.FEATURE_OF_INTEREST);
     }
 
-    public void readRelatedEntityOfEntityWithEntityType(EntityType entityType) {
+    /**
+     * This helper method is the start point for testing resource path.
+     * It adds the entity type to be tested to resource path chain and call the other method to test the chain.
+     * @param entityType Entity type from EntityType enum list
+     */
+    private void readRelatedEntityOfEntityWithEntityType(EntityType entityType) {
         List<String> entityTypes = new ArrayList<>();
         List<Long> ids = new ArrayList<>();
         switch (entityType) {
@@ -199,7 +248,12 @@ public class Capability1Tests {
         readRelatedEntity(entityTypes,ids);
     }
 
-    public void readRelatedEntity(List<String> entityTypes, List<Long> ids){
+    /**
+     * This helper method is testing the chain to the specified level. It confirms that the response is 200.
+     * @param entityTypes List of entity type from EntityType enum list for the chain
+     * @param ids List of ids for teh chain
+     */
+    private void readRelatedEntity(List<String> entityTypes, List<Long> ids){
         if(entityTypes.size() > resourcePathLevel){
             return;
         }
@@ -237,7 +291,14 @@ public class Capability1Tests {
 
     }
 
-    public void checkAssociationLinks(String response, List<String> entityTypes, List<Long> ids) {
+    /**
+     * This method is checking the response for the request of Association Link.
+     * It confirms that it contains a list of selfLinks.
+     * @param response The response for GET association link request
+     * @param entityTypes List of entity type from EntityType enum list for the chain
+     * @param ids List of ids for teh chain
+     */
+    private void checkAssociationLinks(String response, List<String> entityTypes, List<Long> ids) {
 
         try {
             Assert.assertTrue(response.indexOf("value") != -1, "The GET entities Association Link response does not match SensorThings API : missing \"value\" in response.: "+entityTypes.toString()+ids.toString());
@@ -259,7 +320,12 @@ public class Capability1Tests {
         }
     }
 
-    public String readEntityWithEntityType(EntityType entityType) {
+    /**
+     * This method is reading a specific entity and return it as a string.
+     * @param entityType Entity type from EntityType enum list
+     * @return The entity response as a string
+     */
+    private String readEntityWithEntityType(EntityType entityType) {
         try {
             String response = getEntities(entityType);
             Long id = new JSONObject(response.toString()).getJSONArray("value").getJSONObject(0).getLong(ControlInformation.ID);
@@ -275,12 +341,19 @@ public class Capability1Tests {
         }
     }
 
-    public void readNonexistentEntityWithEntityType(EntityType entityType) {
+    /**
+     * This method is check the response of sending a GET request to m=nonexistent entity is 404.
+     * @param entityType Entity type from EntityType enum list
+     */
+    private void readNonexistentEntityWithEntityType(EntityType entityType) {
         long id = Long.MAX_VALUE;
         int responseCode = Integer.parseInt(getEntity(entityType, id, null).get("response-code").toString());
         Assert.assertEquals(responseCode, 404, "Reading non-exitixting " + entityType.name() + " with id " + id + " failed.");
     }
 
+    /**
+     * This method is testing the root URL of the service under test. It basically checks the first page.
+     */
     @Test(description = "Check Service Root UI", groups = "level-1")
     public void checkServiceRootUri() {
         try {
@@ -362,7 +435,12 @@ public class Capability1Tests {
         }
     }
 
-    public String getEntities(EntityType entityType) {
+    /**
+     * This helper method is sending GET request to a collection of entities.
+     * @param entityType Entity type from EntityType enum list
+     * @return The response of GET request in string format.
+     */
+    private String getEntities(EntityType entityType) {
         String urlString = rootUri;
         if(entityType!= null) {
             urlString = ServiceURLBuilder.buildURLString(rootUri, entityType, -1, null, null);
@@ -379,7 +457,14 @@ public class Capability1Tests {
         return response.toString();
     }
 
-    public Map<String,Object> getEntity(EntityType entityType, long id, String property) {
+    /**
+     * This helper method is sending Get request to a specific entity
+     * @param entityType Entity type from EntityType enum list
+     * @param id The if of the specific entity
+     * @param property The requested property of the entity
+     * @return The response-code and response (body) of the request in Map format.
+     */
+    private Map<String,Object> getEntity(EntityType entityType, long id, String property) {
         if (id == -1) {
             return null;
         }
@@ -387,21 +472,34 @@ public class Capability1Tests {
         return HTTPMethods.doGet(urlString);
     }
 
-    public void checkEntitiesAllAspectsForResponse(EntityType entityType, String response){
+    /**
+     * This helper method is the start point for checking the response for a collection in all aspects.
+     * @param entityType Entity type from EntityType enum list
+     * @param response The response of the GET request to be checked
+     */
+    private void checkEntitiesAllAspectsForResponse(EntityType entityType, String response){
         checkEntitiesControlInformation(response);
         checkEntitiesProperties(entityType, response);
         checkEntitiesRelations(entityType, response);
     }
 
-    public void checkEntityAllAspectsForResponse(EntityType entityType, String response){
+    /**
+     * This helper method is the start point for checking the response for a specific entity in all aspects.
+     * @param entityType Entity type from EntityType enum list
+     * @param response  The response of the GET request to be checked
+     */
+    private void checkEntityAllAspectsForResponse(EntityType entityType, String response){
         checkEntityControlInformation(response);
         checkEntityProperties(entityType, response);
         checkEntityRelations(entityType, response);
     }
 
 
-
-    public void checkEntitiesControlInformation(String response){
+    /**
+     * This helper method is checking the control information of the response for a collection
+     * @param response The response of the GET request to be checked
+     */
+    private void checkEntitiesControlInformation(String response){
         try {
             JSONObject jsonResponse = new JSONObject(response.toString());
             JSONArray entities = jsonResponse.getJSONArray("value");
@@ -417,7 +515,11 @@ public class Capability1Tests {
         }
     }
 
-    public void checkEntityControlInformation(Object response){
+    /**
+     * This helper method is checking the control information of the response for a specific entity
+     * @param response The response of the GET request to be checked
+     */
+    private void checkEntityControlInformation(Object response){
         try {
             JSONObject entity = new JSONObject(response.toString());
             try {
@@ -436,7 +538,12 @@ public class Capability1Tests {
         }
     }
 
-    public void checkEntitiesProperties(EntityType entityType, String response){
+    /**
+     * This helper method is checking the mandatory properties of the response for a collection
+     * @param entityType Entity type from EntityType enum list
+     * @param response The response of the GET request to be checked
+     */
+    private void checkEntitiesProperties(EntityType entityType, String response){
         try {
             JSONObject jsonResponse = new JSONObject(response.toString());
             JSONArray entities = jsonResponse.getJSONArray("value");
@@ -454,7 +561,12 @@ public class Capability1Tests {
 
     }
 
-    public void checkEntityProperties(EntityType entityType, Object response){
+    /**
+     * This helper method is checking the mandatory properties of the response for a specific entity
+     * @param entityType Entity type from EntityType enum list
+     * @param response The response of the GET request to be checked
+     */
+    private void checkEntityProperties(EntityType entityType, Object response){
         try {
             JSONObject entity = new JSONObject(response.toString());
             for (String property : EntityProperties.getPropertiesListFor(entityType)) {
@@ -472,7 +584,12 @@ public class Capability1Tests {
 
     }
 
-    public void checkEntitiesRelations(EntityType entityType, String response){
+    /**
+     * This helper method is checking the mandatory relations of the response for a collection
+     * @param entityType Entity type from EntityType enum list
+     * @param response The response of the GET request to be checked
+     */
+    private void checkEntitiesRelations(EntityType entityType, String response){
         try {
             JSONObject jsonResponse = new JSONObject(response);
             JSONArray entities = jsonResponse.getJSONArray("value");
@@ -489,7 +606,12 @@ public class Capability1Tests {
 
     }
 
-    public void checkEntityRelations(EntityType entityType, Object response){
+    /**
+     * This helper method is checking the mandatory relations of the response for a specific entity
+     * @param entityType Entity type from EntityType enum list
+     * @param response The response of the GET request to be checked
+     */
+    private void checkEntityRelations(EntityType entityType, Object response){
         try {
             JSONObject entity = new JSONObject(response.toString());
             for (String relation : EntityRelations.getRelationsListFor(entityType)) {
