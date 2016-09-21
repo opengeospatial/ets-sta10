2ff0fc9b79fe31830c79d55388a973b10321e8e3
Hylke van der Schaaf <hylke.vanderschaaf@iosb.fraunhofer.de>
2016-09-21 17:35:42 +0200

    Added second missing set of NotEqual tests.

--- src/main/java/org/opengis/cite/sta10/filteringExtension/Capability3Tests.java
+++ src/main/java/org/opengis/cite/sta10/filteringExtension/Capability3Tests.java
@@ -1818,6 +1818,11 @@ public class Capability3Tests {
                 response = responseMap.get("response").toString();
                 checkPropertiesForFilter(response, filteredProperties, samplePropertyValues, 0);
 
+                urlString = ServiceURLBuilder.buildURLString(rootUri, entityType, id, relationEntityType, "?$filter=" + property + "%20ne%20" + propertyValue);
+                responseMap = HTTPMethods.doGet(urlString);
+                response = responseMap.get("response").toString();
+                checkPropertiesForFilter(response, filteredProperties, samplePropertyValues, -3);
+
                 urlString = ServiceURLBuilder.buildURLString(rootUri, entityType, id, relationEntityType, "?$filter=" + property + "%20ge%20" + propertyValue);
                 responseMap = HTTPMethods.doGet(urlString);
                 response = responseMap.get("response").toString();
