236b689738ac5628e555ad7ccf9bc34b7cd1583a
Hylke van der Schaaf <hylke.vds@gmail.com>
2016-10-16 17:02:30 +0200

    * Added tests for result types boolean, array and object. * Moved result-type test to separate class.

--- src/main/java/org/opengis/cite/sta10/createUpdateDelete/Capability2Tests.java
+++ src/main/java/org/opengis/cite/sta10/createUpdateDelete/Capability2Tests.java
@@ -193,9 +193,6 @@ public class Capability2Tests {
             entity = postEntity(EntityType.OBSERVATION, urlParameters);
             long obsId1 = entity.getLong(ControlInformation.ID);
             observationIds.add(obsId1);
-            // Check the type of result, should be numeric.
-            Object result = entity.get("result");
-            Assert.assertTrue(result instanceof Number, "Numeric result no longer Numeric!");
 
             //POST Observation without FOI (Automatic creation of FOI)
             //Add location to the Thing
@@ -205,7 +202,7 @@ public class Capability2Tests {
             urlParameters = "{\n"
                     + "  \"phenomenonTime\": \"2015-03-01T00:00:00.000Z\",\n"
                     + "  \"resultTime\": \"2015-03-01T01:00:00.000Z\",\n"
-                    + "  \"result\": \"100\",\n"
+                    + "  \"result\": 100,\n"
                     + "  \"Datastream\":{\"@iot.id\": " + datastreamId + "}\n"
                     + "}";
             entity = postEntity(EntityType.OBSERVATION, urlParameters);
@@ -214,9 +211,6 @@ public class Capability2Tests {
             observationIds.add(obsId2);
             long automatedFOIId = checkAutomaticInsertionOfFOI(obsId2, locationEntity, -1);
             foiIds.add(automatedFOIId);
-            // Check the type of result, should be String.
-            result = entity.get("result");
-            Assert.assertTrue(result instanceof String, "String result no longer String!");
 
             //POST another Observation to make sure it is linked to the previously created FOI
             urlParameters = "{\n"
